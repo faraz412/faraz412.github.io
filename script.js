fe38e7667f63483c80951825995aee55a7d785fd
faraz412 <ahmedfaraz412@gmail.com>
2023-02-15 23:53:32 +0530

    commit 15-02-23 (5)

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ const resumeBtn2=document.getElementById("resume-button-2");
 
 resumeBtn1.addEventListener("click",(e)=>{
     setTimeout(()=>{
-        window.open("https://drive.google.com/file/d/1AcxnYYI7awYPNpPfYKPZAK_I_cW6gWmD/view?usp=sharing","_blank");
+        window.open(window.location.href);
+        window.location.href = "https://drive.google.com/file/d/1AcxnYYI7awYPNpPfYKPZAK_I_cW6gWmD/view?usp=sharing";
     },500);
 })
 
 resumeBtn2.addEventListener("click",(e)=>{
     setTimeout(()=>{
-        window.open("https://drive.google.com/file/d/1AcxnYYI7awYPNpPfYKPZAK_I_cW6gWmD/view?usp=sharing","_blank");
+        window.open(window.location.href);
+        window.location.href = "https://drive.google.com/file/d/1AcxnYYI7awYPNpPfYKPZAK_I_cW6gWmD/view?usp=sharing";
     },500);
 })
