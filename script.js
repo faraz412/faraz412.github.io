e6d03c568d0d11621bb74006a3dc58ab3767c306
faraz412 <ahmedfaraz412@gmail.com>
2023-02-15 23:57:43 +0530

    commit 15-02-23 (6)

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const resumeBtn1=document.getElementById("resume-button-1");
 const resumeBtn2=document.getElementById("resume-button-2");
 
-resumeBtn1.addEventListener("click",(e)=>{
-    setTimeout(()=>{
-        window.open(window.location.href);
-        window.location.href = "https://drive.google.com/file/d/1AcxnYYI7awYPNpPfYKPZAK_I_cW6gWmD/view?usp=sharing";
-    },500);
-})
+// resumeBtn1.addEventListener("click",(e)=>{
+//     setTimeout(()=>{
+//         window.open(window.location.href);
+//         window.location.href = "https://drive.google.com/file/d/1AcxnYYI7awYPNpPfYKPZAK_I_cW6gWmD/view?usp=sharing";
+//     },500);
+// })
 
-resumeBtn2.addEventListener("click",(e)=>{
-    setTimeout(()=>{
-        window.open(window.location.href);
-        window.location.href = "https://drive.google.com/file/d/1AcxnYYI7awYPNpPfYKPZAK_I_cW6gWmD/view?usp=sharing";
-    },500);
-})
+// resumeBtn2.addEventListener("click",(e)=>{
+//     setTimeout(()=>{
+//         window.open(window.location.href);
+//         window.location.href = "https://drive.google.com/file/d/1AcxnYYI7awYPNpPfYKPZAK_I_cW6gWmD/view?usp=sharing";
+//     },500);
+// })
