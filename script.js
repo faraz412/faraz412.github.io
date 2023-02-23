03a5323f890f99b3177c4befec5440e662d3b08a
faraz412 <ahmedfaraz412@gmail.com>
2023-02-24 02:51:01 +0530

    commit 24-02-23 (1)

--- script.js
+++ script.js
@@ -14,3 +14,14 @@ const resumeBtn2=document.getElementById("resume-button-2");
 //         window.location.href = "https://drive.google.com/file/d/1AcxnYYI7awYPNpPfYKPZAK_I_cW6gWmD/view?usp=sharing";
 //     },500);
 // })
+
+const hamburger=document.getElementById("hamb");
+const navbar_menu=document.getElementById("navbar-menu");
+
+hamburger.addEventListener("click", (e)=>{
+    if(navbar_menu.style.display=="none"){
+        navbar_menu.style.display="block";
+    }else{
+        navbar_menu.style.display="none";
+    }
+})
